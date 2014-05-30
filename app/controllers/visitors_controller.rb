627020dbbbbb8bd2ea66d298a8446508129ef0a1
Jack Hou <jack.hou@Live.com>
2014-05-30 11:04:19 -0700

    Update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,5 +3,7 @@ class VisitorsController < ApplicationController
 	#be available in view.html.erb
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = "Welcome!"
+		flash.now[:alert] = "My birthday is soon!"
 	end
 end
